8b7a1f11dc0613938ebe5d0554ab397e0d623732
DodiDevrian <dodi.119140023@student.itera.ac.id>
2023-08-12 16:13:12 +0700

    Perubahan Tampilan Back End

---
+++ application/views/admin/layout2/v_footer.php
@@ -0,0 +1,41 @@
+</div>
+<!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="<?php base_url() ?>template/back-end/js/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php base_url() ?>template/back-end/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?php base_url() ?>template/back-end/js/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?php base_url() ?>template/back-end/js/startmin.js"></script>
+
+<!-- DataTables JavaScript -->
+<script src="<?php base_url() ?>template/back-end/js/dataTables/jquery.dataTables.min.js"></script>
+<script src="<?php base_url() ?>template/back-end/js/dataTables/dataTables.bootstrap.min.js"></script>
+
+
+<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+</script>
+
+</body>
+
+</html>
